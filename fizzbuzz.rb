8d0635489e86e364df7e9316352d4e0e95acbbb8
William Falcone <wjf0003+will@gmail.com>
2018-01-18 20:03:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0  
     puts "Fizzbuzz"
-  end
   elsif int % 3 == 0 
     puts "Fizz"
-  end
   elsif int % 5 == 0 
     puts "Buzz"
-  end
   else
     puts "nil"
   end 
